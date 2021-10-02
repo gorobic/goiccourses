da4b6d9ae0c91e2ba8c2fe869644734d57ad793a
gorobic <gorobic@gmail.com>
2021-10-02 11:13:11 +0300

    footer sidebar changes

--- template-parts/footer/footer-widgets.php
+++ template-parts/footer/footer-widgets.php
@@ -1,20 +1,28 @@
-<?php if ( is_active_sidebar( 'footer-widgets' )){ ?>
+<?php if ( is_active_sidebar( 'footer-widgets-1' ) || is_active_sidebar( 'footer-widgets-2' ) || is_active_sidebar( 'footer-widgets-3' ) || is_active_sidebar( 'footer-widgets-4' ) ){ ?>
 <div class="bg-dark text-light footer-widgets overflow-hidden">
     <div class="container mt-5 mb-4">
         <aside class="widget-area row" role="complementary"
             aria-label="<?php esc_attr_e( 'Footer', 'text_domain' ); ?>">
-            <div class="col-sm-6 col-lg-3">
-                <?php dynamic_sidebar( 'footer-widgets-1' ); ?>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <?php dynamic_sidebar( 'footer-widgets-2' ); ?>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <?php dynamic_sidebar( 'footer-widgets-3' ); ?>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <?php dynamic_sidebar( 'footer-widgets-4' ); ?>
-            </div>
+            <?php if ( is_active_sidebar( 'footer-widgets-1' )){ ?>
+                <div class="col-sm-6 col-lg-3">
+                    <?php dynamic_sidebar( 'footer-widgets-1' ); ?>
+                </div>
+            <?php } ?>
+            <?php if ( is_active_sidebar( 'footer-widgets-2' )){ ?>
+                <div class="col-sm-6 col-lg-3">
+                    <?php dynamic_sidebar( 'footer-widgets-2' ); ?>
+                </div>
+            <?php } ?>
+            <?php if ( is_active_sidebar( 'footer-widgets-3' )){ ?>
+                <div class="col-sm-6 col-lg-3">
+                    <?php dynamic_sidebar( 'footer-widgets-3' ); ?>
+                </div>
+            <?php } ?>
+            <?php if ( is_active_sidebar( 'footer-widgets-4' )){ ?>
+                <div class="col-sm-6 col-lg-3">
+                    <?php dynamic_sidebar( 'footer-widgets-4' ); ?>
+                </div>
+            <?php } ?>
         </aside><!-- .widget-area -->
     </div>
 </div>
