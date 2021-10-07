49f35dcb78936915ea1d22919779fb120b1096a9
gorobic <gorobic@gmail.com>
2021-10-07 23:51:23 +0300

    translation text domain update for user tab

--- template-parts/user/user-tab-account.php
+++ template-parts/user/user-tab-account.php
@@ -1,12 +1,12 @@
 <form method="post" id="adduser" action="<?php echo home_url().$_SERVER['REQUEST_URI']; ?>">
     <p class="form-username">
-        <label for="username"><?php _e('User name', 'profile'); ?></label>
+        <label for="username"><?php _e('User name', 'goicc'); ?></label>
         <input class="text-input form-control" name="username" type="text" id="username" disabled
             value="<?php the_author_meta( 'user_login', $user_id ); ?>" />
         <small class="form-text text-muted"><?php _e('Usernames cannot be changed.', 'goicc'); ?></small>
     </p>
     <p class="form-email">
-        <label for="email"><?php _e('Email *', 'profile'); ?></label>
+        <label for="email"><?php _e('Email *', 'goicc'); ?></label>
         <input class="text-input form-control" name="email" type="text" id="email"
             value="<?php the_author_meta( 'user_email', $user_id ); ?>" required />
     </p>
@@ -17,17 +17,17 @@
     </h4>
 
     <p class="form-password">
-        <label for="current_password"><?php _e('Current Password', 'profile'); ?> </label>
+        <label for="current_password"><?php _e('Current Password', 'goicc'); ?> </label>
         <input class="text-input form-control" name="current_password" type="password" id="current_password" />
         <small class="form-text text-muted"><?php _e('Leave blank to leave unchanged', 'goicc'); ?></small>
     </p><!-- .form-password -->
     <p class="form-password">
-        <label for="pass1"><?php _e('New Password', 'profile'); ?> </label>
+        <label for="pass1"><?php _e('New Password', 'goicc'); ?> </label>
         <input class="text-input form-control" name="pass1" type="password" id="pass1" />
         <small class="form-text text-muted"><?php _e('Leave blank to leave unchanged', 'goicc'); ?></small>
     </p><!-- .form-password -->
     <p class="form-password">
-        <label for="pass2"><?php _e('Repeat New Password', 'profile'); ?> </label>
+        <label for="pass2"><?php _e('Repeat New Password', 'goicc'); ?> </label>
         <input class="text-input form-control" name="pass2" type="password" id="pass2" />
         <small class="form-text text-muted"><?php _e('Leave blank to leave unchanged', 'goicc'); ?></small>
     </p><!-- .form-password -->
@@ -39,7 +39,7 @@
     <p class="form-submit">
         <?php echo $referer; ?>
         <input name="updateuser" type="submit" id="updateuser" class="submit button btn btn-primary"
-            value="<?php _e('Update', 'profile'); ?>" />
+            value="<?php _e('Update', 'goicc'); ?>" />
         <?php wp_nonce_field( 'update-user' ) ?>
         <input name="action" type="hidden" id="action" value="update-user" />
     </p><!-- .form-submit -->
